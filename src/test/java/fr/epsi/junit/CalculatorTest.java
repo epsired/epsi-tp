f48b9848976e6775c3d4edf770b667ed6c022e41
Raphaël VINOTTI <raphael.vinotti@epsi.fr>
2021-06-21 12:15:55 +0200

    push1

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -1,21 +1,32 @@
 package fr.epsi.junit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class CalculatorTest {
 
     private Calculator calculator;
-
+    @BeforeEach
     public void init(){
+        this.calculator = new Calculator();
         //TODO
+
     }
 
     public void should_add_two_numbers(){
+        Scanner sca = new Scanner(System.in);
+        System.out.println("Veuillez saisir un nombre a :");
+        int a = sca.nextInt();
+        Scanner scb = new Scanner(System.in);
+        System.out.println("Veuillez saisir un nombre b :");
+        int b = scb.nextInt();
        // TODO
     }
 
     public void should_substract_two_numbers(){
+
         // TODO
     }
 
